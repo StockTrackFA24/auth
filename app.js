ae666530755896e4b11c3a8db7cd77af5b052b40
bigfoot547 <bigfoot@figboot.dev>
2024-11-27 03:04:07 -0600

    password validation added

--- app.js
+++ app.js
@@ -1,13 +1,51 @@
 var express = require('express');
 var logger = require('morgan');
 
+const { MongoClient } = require('mongodb');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+/* set up mongodb */
+const mongo = new MongoClient(process.env.MONGODB_URI);
+
+const db = mongo.db();
+const users = db.collection('users');
+const roles = db.collection('roles');
+
+(async () => {
+  await mongo.connect();
+  console.log("yay! mongodb is connected");
+
+  await users.createIndexes([
+    {
+      key: { username: 1 },
+      unique: true
+    }
+  ]);
+
+  await roles.createIndexes([
+    {
+      key: { name: 1 },
+      unique: true
+    }
+  ]);
+
+  console.log("mongodb indices created.");
+})().catch((e) => {
+  console.dir(e);
+  process.exit(2);
+});
 
 var app = express();
 
 app.use(logger('dev'));
 app.use(express.json({ type: '*/*' }));
+
+app.use((req, res, next) => {
+  req.collections = { users: users, roles: roles };
+  return next();
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const argon2 = require('argon2');
 var router = express.Router();
 
 /* user sample
@@ -13,13 +14,15 @@ var router = express.Router();
  *                                     Prefer using it over constructing their full name from the "name" object.
  *   "password": String,
  *   "passwordChanged": DateTime,
+ *   "lastLogin": DateTime,
  *   "username": String, -- Frontend note: This will have the following restrictions imposed by the auth service:
  *                                         - must start with a lowercase letter
  *                                         - may contain 2 through 31 additional lowercase letters, numbers, underscores (_), or full stops (.).
+ *                                         You are recommended to normalize (i.e. case-correct) usernames before passing them to the auth service.
  *   "loginDisabled": String || null, -- null indicates the user is allowed to log in, or the reason the user is forbidden to log in if present.
  *   "adminNotes": String || null,
  *
- *   "roleIds": [ObjectId] -- Use MongoDB addToSet to add roles here.
+ *   "roles": [ObjectId] -- Use MongoDB addToSet to add roles here.
  * }
  *
  * role sample
@@ -37,6 +40,7 @@ var router = express.Router();
  */
 
 const VALID_USERNAME_PATTERN = /^[a-z][a-z0-9_.]{2,31}$/
+const PASSWORD_SECRET = Buffer.from(process.env.PASSWORD_SECRET);
 
 /* GET users listing. */
 router.post('/login', async (req, res, next) => {
@@ -62,7 +66,64 @@ router.post('/login', async (req, res, next) => {
 
   // ... look up user by username ...
 
+  const GENERIC_AUTH_ERROR = {status: 403, message: 'Invalid username/password'};
+  const doSleep = (ms) => (new Promise((resolve, _) => setTimeout(resolve, ms)));
+  const user = await req.collections.users.findOne({username: {$eq: req.body.username}});
 
+  // ... try to make sure you can't use this endpoint as an oracle for whether a username exists by fuzzing the timings a bit ...
+  await doSleep(Math.random() * 250);
+
+  if (!user || !user.password) {
+    await doSleep(100);
+    return next(GENERIC_AUTH_ERROR);
+  }
+
+  // make sure the password is valid
+
+  try {
+    if (!await argon2.verify(user.password, req.body.password, { secret: PASSWORD_SECRET })) {
+      return next(GENERIC_AUTH_ERROR);
+    }
+  } catch (e) {
+    console.dir(e);
+    return next({status: 500, message: "Internal error"});
+  }
+
+  // make sure they are actually allowed to log in
+  /* (the rationale behind doing this *after* the server has spent resources on password validation is that I don't want
+   *  randos to be able to check if a user is forbidden from logging in without actually getting the password correct) */
+
+  if (typeof user.loginDisabled === 'string') {
+    if (user.loginDisabled) {
+      // XSS WARNING FOR FRONTEND!!!!! Do not just put this string on the page! Make sure to sanitize it first!!
+      return next({status: 403, message: "Your account is disabled: " + user.loginDisabled});
+    } else {
+      return next({status: 403, message: "Your account is disabled."});
+    }
+  }
+
+  const perms = await req.collections.users.aggregate([
+    {
+      $match: { _id: user._id }
+    },
+    {
+      $graphLookup: {
+        from: 'roles',
+        startWith: '$roles',
+        connectFromField: 'inherit',
+        connectToField: '_id',
+        as: 'allRoles',
+        depthField: 'depth'
+      }
+    },
+    {
+      $unwind: '$allRoles'
+    }
+  ]).next();
+
+  console.log(perms);
+
+  return next({status: 501, message: "not implemented"});
 });
 
 module.exports = router;
